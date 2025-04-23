142d7b53160b65df5369379ae0b2194a12200e11
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-04-23 13:26:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-
-    })
-    return config;
-  },
-
   typescript: {
    
     ignoreBuildErrors: true,
@@ -21,6 +12,26 @@ const nextConfig: NextConfig = {
 
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+
+    })
+
+    // Add this block:
+  config.ignoreWarnings = [
+    (warning) =>
+      typeof warning.message === 'string' &&
+      (
+        warning.message.includes("Can't resolve './ladywithLaptop.svg'") ||
+        warning.message.includes("Can't resolve '@/components/IdeaContainer'")
+      )
+  ];
+
+    return config;
+  },
+
 
  async redirects(){
   return [
