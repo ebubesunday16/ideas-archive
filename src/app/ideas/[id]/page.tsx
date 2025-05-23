494d6545be5f5209a93f05029fe52a5390c484d1
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-05-23 12:12:37 +0100

    Update page.tsx

--- src/app/ideas/[id]/page.tsx
+++ src/app/ideas/[id]/page.tsx
@@ -25,7 +25,7 @@ const Page = async ({ params } : {params: {id: number}}) => {
   if (!ideaData) return notFound();
   
   return (
-    <div className="">
+    <div className="min-h-screen  text-white relative overflow-hidden">
       {/* Background Effects */}
       <div className="absolute inset-0">
         <div className="absolute top-0 left-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl"></div>
