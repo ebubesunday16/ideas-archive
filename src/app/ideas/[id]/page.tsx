73abf779586daee0a2503807be953e655cfc834a
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-05-26 17:47:16 +0100

    Update page.tsx

--- src/app/ideas/[id]/page.tsx
+++ src/app/ideas/[id]/page.tsx
@@ -26,12 +26,12 @@ const Page = async ({ params } : {params: {id: number}}) => {
   
   return (
     <div className="min-h-screen  text-white relative overflow-hidden">
-      {/* Background Effects */}
+      {/* Background Effects
       <div className="absolute inset-0">
         <div className="absolute top-0 left-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl"></div>
         <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-gradient-to-r from-purple-500/5 to-blue-500/5 rounded-full blur-3xl"></div>
-      </div>
+      </div> */}
 
       {/* Hero Section */}
       <section className="relative z-10 pt-20 pb-16">
